4e68faf0d1abf81e8a3fc7bf79846df2fdf3383d
blake-xiao <gme.usm@gmail.com>
2014-01-19 16:22:05 -0500

    Initial commit
    This is the first commit done through github windows client - - only has
    the minimal files to track.

---
+++ README.txt
@@ -0,0 +1,1 @@
+This is just a sample project for me to learn the capability of git. Nothing important really.
---
+++ src/main/java/hello/Greeter.java
@@ -0,0 +1,10 @@
+package hello;
+/**
+* Add some javadoc for this class to check the diff command.
+*/
+public class Greeter {
+	// do something useful at least
+    public String sayHello() {
+        return "Hello world!";
+    }
+}
